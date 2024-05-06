6afd1728f367f2122debd917334f1405cb1d2a58
Road-block <dridzt.addons@hotmail.com>
2024-05-06 22:06:28 +0300

    de-globalization, use the vararg passed in by the client addon name is a good global as it's guaranteed to be unique at the folder level at least

--- TOM_Util.lua
+++ TOM_Util.lua
@@ -1,4 +1,5 @@
-TOM = {}
+local addonName, TOM = ...
+
 TOM.DROPDOWN_RENAME = 1
 TOM.DROPDOWN_DELETE = 2
 TOM.SLOTID_TO_NAME = {
@@ -17,16 +18,16 @@ TOM.SLOTID_TO_NAME = {
 		[18] = "RANGEDSLOT",
 		[19] = "TABARDSLOT"
 }
-MyOutfits = {}
+TransmogOutfitManagerDB = TransmogOutfitManagerDB or {}
 
-function TOM_GetTransmogId(slot)
+function TOM.GetTransmogId(slot)
 	if slot.hasUndo then return tonumber(slot.base)
 	elseif slot.pending > 0 then return tonumber(slot.pending)
 	elseif slot.applied > 0 then return tonumber(slot.applied)
 	else return tonumber(slot.base) end
 end
 
-function TOM_IsValidName(name)
+function TOM.IsValidName(name)
 	local nameLength = string.len(name)
 	if nameLength == 0 or nameLength > 15 then
 		return false
@@ -35,16 +36,16 @@ function TOM_IsValidName(name)
 	end
 end
 
-function TOM_NumSavedOutfits()
+function TOM.NumSavedOutfits()
 	local count = 0
-	for _, outfit in pairs(MyOutfits) do
+	for _, outfit in pairs(TransmogOutfitManagerDB) do
 		if outfit.name then count = count + 1 end
 	end
 	return count
 end
 
-function TOM_OutfitExistsByName(name)
-	for _, outfit in pairs(MyOutfits) do
+function TOM.OutfitExistsByName(name)
+	for _, outfit in pairs(TransmogOutfitManagerDB) do
 		if outfit.name == name then return true end
 	end
 	return false
